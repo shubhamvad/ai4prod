cb60b5553adaef045c67a379672bbff4f1a23212
Eric Tondelli <eric.tondelli@gmail.com>
2021-03-17 18:13:47 +0100

    add test integration

--- test/testClassification.cpp
+++ test/testClassification.cpp
@@ -1,14 +1,28 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "classification.h"
+using namespace ai4prod;
+using namespace classification;
 
 
 unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
 }
 
+
+
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( Factorial(1) == 1 );
     REQUIRE( Factorial(2) == 2 );
     REQUIRE( Factorial(3) == 6 );
     REQUIRE( Factorial(10) == 3628800 );
+}
+
+TEST_CASE("Init classification"){
+
+    ResNet50 *resnet=new ResNet50();
+    
+    REQUIRE(1==1);
+
+
 }
