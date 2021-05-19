d06f9dbbf2df91bd9d65d7d0bae8518b97567692
eric-win <eric.tondelli@gmail.com>
2021-05-19 15:20:01 +0200

    test on Windows install script

--- installWin.bat
+++ installWin.bat
@@ -1,46 +1,58 @@
 
-@REM IF "%1"=="--cuda" (
-@REM         SET cuda=%2
-@REM     )
+IF "%1"=="--cuda" (
+        SET cuda=%2
+    )
 
-@REM @ECHO OFF
-@REM ECHO Congratulations! Your first batch file was executed successfully.
-@REM ECHO %cuda%
+@ECHO OFF
+ECHO Congratulations! Your first batch file was executed successfully.
+ECHO %cuda%
 
-@REM IF "%cuda%"=="10.2" (
-@REM     ECHO Select cuda 10.2
-@REM     set fileid="1h1ybEJWrysYJ8PKCdL9ELEfUNk7Nn1Qa"
-@REM     )
+IF "%cuda%"=="10.2" (
+    ECHO Select cuda 10.2
+    set fileid="1h1ybEJWrysYJ8PKCdL9ELEfUNk7Nn1Qa"
+    )
 
+set cookieFile=cookie.txt
+set confirmFile=confirm.txt
 
+set filename="deps.7z"
 
-@REM set cookieFile=cookie.txt
-@REM set confirmFile=confirm.txt
-
-@REM set filename="deps.7z"
-
-@REM REM downlaod cooky and message with request for confirmation
-@REM wget --quiet --save-cookies "%cookieFile%" --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=%fileid%" -O "%confirmFile%"
+REM downlaod cooky and message with request for confirmation
+wget --quiet --save-cookies "%cookieFile%" --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=%fileid%" -O "%confirmFile%"
 
 
-@REM REM extract confirmation key from message saved in confirm file and keep in variable resVar
-@REM call jrepl ".*confirm=([0-9A-Za-z_]+).*" "$1" /F "confirm.txt" /A /rtn resVar
+REM extract confirmation key from message saved in confirm file and keep in variable resVar
+call jrepl ".*confirm=([0-9A-Za-z_]+).*" "$1" /F "confirm.txt" /A /rtn resVar
 
-@REM ECHO %resVar%
+ECHO %resVar%
 
-@REM REM when jrepl writes to variable, it adds carriage return (CR) (0x0D) and a line feed (LF) (0x0A), so remove these two last characters
-@REM SET confirmKey=%resVar%:~0,-2%
+REM when jrepl writes to variable, it adds carriage return (CR) (0x0D) and a line feed (LF) (0x0A), so remove these two last characters
+SET confirmKey=%resVar%:~0,-2%
 
-@REM ECHO %confirmKey%
-@REM REM download the file using cookie and confirmation key
-@REM wget --load-cookies "%cookieFile%" -O "%filename%" "https://docs.google.com/uc?export=download&id=%fileid%&confirm=%confirmKey%"
+ECHO %confirmKey%
+REM download the file using cookie and confirmation key
+wget --load-cookies "%cookieFile%" -O "%filename%" "https://docs.google.com/uc?export=download&id=%fileid%&confirm=%confirmKey%"
 
-@REM REM clear temporary files 
-@REM del %cookieFile%
-@REM del %confirmFile%
+REM clear temporary files 
+del %cookieFile%
+del %confirmFile%
 
-@REM Unzip deps
+REM Unzip deps. Could take a while
 
+mkdir deps
+cd deps
 set PATH=%PATH%;C:\Program Files\7-Zip\
 echo %PATH%
-7z deps.7z
+7z x ..\deps.7z
+
+move tensorrt C:\
+
+REM Install vcpkg
+git clone https://github.com/microsoft/vcpkg
+cd vcpkg
+git checkout 00d190a039c78cad4aadfc9a9f3ce8b53165ae6b
+
+call bootstrap-vcpkg.bat
+
+
+
